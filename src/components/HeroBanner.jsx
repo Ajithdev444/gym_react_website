9eeb365484ee3ce3faf49919968823ded38d313d
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-19 17:03:35 +0530

    Navbar

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Stack, Box, Typography } from '@mui/material'
+
+const HeroBanner = () => {
+  return (
+    <div>HeroBanner</div>
+  )
+}
+
+export default HeroBanner
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react'
+import { Box } from '@mui/material'
+import HeroBanner from '../components/HeroBanner'
+import SearchExercises from '../components/SearchExercises'
+import Exercises from '../components/Exercises'
+
+const Home = () => {
+  return (
+    <Box>
+      <HeroBanner/>
+      <SearchExercises/>
+      <Exercises/>
+    </Box>
+  )
+}
+
+export default Home
