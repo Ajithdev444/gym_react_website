4aaaa514604a7001bc41c2cca3149c0720d93ed5
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-19 17:18:09 +0530

    Box Typographt style

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -3,7 +3,13 @@ import { Stack, Box, Typography } from '@mui/material'
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <Box sx={{ mt:{ lg:'212px', xs:'70px'},
+               ml:{ sm:'50px'}}} 
+               position='relative'>
+        <Typography color='#FF2625' fontWeight='600' fontSize='26px'>
+            Fitness Club
+        </Typography>
+    </Box>
   )
 }
 
