98519f9f58db1da9a21b543ef5c99f2cb4d502cf
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-20 17:14:33 +0530

    HorizontalScrollBar

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,27 +1,45 @@
-import React from 'react'
-import { Box } from '@mui/material'
-import { ScrollMenu, VisibilityContent } from 'react-horizontal-scrolling-menu';
+import React, { useContext } from 'react';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { Box, Typography } from '@mui/material';
 
-import BodyPart from './BodyPart'
+import ExerciseCard from './ExerciseCard';
+import BodyPart from './BodyPart';
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
-const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
 
-  
   return (
-    <ScrollMenu>
-        {data.map((item) =>{ 
-        <Box 
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
+  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+    {data.map((item) => (
+      <Box
         key={item.id || item}
         itemId={item.id || item}
         title={item.id || item}
-        m = '0 40px'
-        >
-          <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
-        </Box>
+        m="0 40px"
+      >
+        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
+      </Box>
+    ))}
+  </ScrollMenu>
+);
 
-         } )}
-    </ScrollMenu>
-  )
-}
-
-export default HorizontalScrollbar
+export default HorizontalScrollbar;
