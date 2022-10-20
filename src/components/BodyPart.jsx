301ed47e892838f2f69f88cac634684c0eede75f
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-20 17:27:22 +0530

    scroll window

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -19,6 +19,10 @@ const BodyPart = ({ item, setBodyPart, bodyPart}) => {
       cursor: 'pointer',
       gap: '47px'
      }}
+     onClick={()=>{
+      setBodyPart=(item);
+      window.scrollTo({ top:1800, left:100, behavior:'smooth'})
+     }}
     >
       <img src={Icon} alt="dumbbell" style={{ width:'40px', height:'40px'}}/>
       <Typography fontSize='24px' fontWeight='bold' color='#3A1212'
