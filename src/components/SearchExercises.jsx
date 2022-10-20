7b13a2ad823a02fb5782f32df9b94e03fc22ec4d
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-20 12:41:21 +0530

    fetching data from exercisedb api

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { Box, Stack, Button, Typography, TextField } from '@mui/material'
+import { exerciseOptions, fetchData} from '../utils/fetchData'
 
 const SearchExercises = () => {
 
   const [search, setSearch] = useState('')
+  const [exercises, setExercises] = useState([])
+  const [bodyParts, setBodyParts] = useState([])
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+      exerciseOptions);
+      setBodyParts(['all',...bodyPartsData]);
+    }
+    fetchExercisesData();
+  }, [])
+  
 
   const handleSearch = async() => {
     if(search) {
-      //const exercisesData = await fetchData();
+      const exercisesData = await fetchData
+      ('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      console.log(exercisesData);
+
+      const searchedExercises = exercisesData.filter(
+        (exercise) => exercise.name.toLowerCase().includes(search)
+        ||  exercise.target.toLowerCase().includes(search)
+        ||  exercise.equipment.toLowerCase().includes(search)
+        ||   exercise.bodyPart.toLowerCase().includes(search)
+      );
+      setSearch('');
+      setExercises(searchedExercises);
     }
 
   }
--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -1,3 +1,7 @@
+export const exerciseOptions = {
+
+}
+
 export const fetchData = async(url, options) => {
     const response = await fetch(url, options)
     const data = await response.json();
