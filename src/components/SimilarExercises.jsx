6353a0226908df16892ba96e564188fb17f40e62
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-21 17:11:41 +0530

    footer

--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { Box, Stack, Typography } from '@mui/material'
+import HorizontalScrollbar from './HorizontalScrollbar'
+import Loader from './Loader'
 
 const SimilarExercises = ({targetMuscleExercises, equipmentExercises}) => {
   return (
     <Box sx={{ mt: { lg:'100px', xs:'0'}}}>
-      <Typography>
+      <Typography variant='h3' mb={5}>
         Exercises that target the same muscle group
       </Typography>
+      <Stack sx={{ p:'2', position:'relative'}}>
+          {targetMuscleExercises.length ? <HorizontalScrollbar data={targetMuscleExercises}/>
+          : <Loader/>}
+      </Stack>
+
+      <Typography variant='h3' mb={5}>
+        Exercises that use the same equipment 
+      </Typography>
+      <Stack sx={{ p:'2', position:'relative'}}>
+          {equipmentExercises.length ? <HorizontalScrollbar data={equipmentExercises}/>
+          : <Loader/>}
+      </Stack>
 
     </Box>
   )
