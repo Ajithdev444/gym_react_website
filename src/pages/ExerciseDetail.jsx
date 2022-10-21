50a670eb3d112fa35075da2f029d6029963e0167
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-21 10:19:58 +0530

    Detail Component

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -20,7 +20,7 @@ const ExerciseDetail = () => {
       const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
       const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
       
-      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`,
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise${id}`,
       exerciseOptions);
 
       setExerciseDetail(exerciseDetailData)
