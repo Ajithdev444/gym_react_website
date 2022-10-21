0d60c8a1932529078787c4971271959eeb46c09f
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-21 09:05:02 +0530

    ExerciseDetail

---
+++ src/components/Detail.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Typography, Stack, Button } from '@mui/material'
+import BodyPartImage from '../assets/icons/body-Part.png'
+import TargetImage from '../assets/icons/target.png'
+import EquipmentImage from '../assets/icons/equipment.png'
+
+
+const Detail = ({exerciseDetail}) => {
+
+  const {} = exerciseDetail
+
+  return (
+    <div>Detail</div>
+  )
+}
+
+export default Detail
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import Detail from '../components/Detail'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercises from '../components/SimilarExercises'
+
 
 const ExerciseDetail = () => {
+
+  const [exerciseDetail,setExerciseDetail] = useState({});
+
+  const { id } = useParams();
+
+  useEffect (() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+      
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`,
+      exerciseOptions);
+
+      setExerciseDetail(exerciseDetailData)
+    }
+    fetchExercisesData();
+  },[id]);
+
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail}/>
+      <ExerciseVideos/>
+      <SimilarExercises/>
+    </Box>
   )
 }
 
