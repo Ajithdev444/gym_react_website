4949e1e710b2532c4c13f4747f3efca132cd04dc
Ajithdev444 <Ajithdev444@gamil.com>
2022-10-21 08:02:11 +0530

    Home page complete

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -20,6 +20,24 @@ const Exercises = ({ exercises, setExercises, bodyPart}) => {
       window.scrollTo({ top:1000, behavior:'smooth'})
     }
 
+    useEffect(() => {
+      const fetchExercisesData = async () => {
+        let exercisesData = [];
+
+        if (bodyPart === 'all') {
+          exercisesData = await fetchData
+          ('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);    
+        } else {
+          exercisesData = await fetchData
+          (`https://exercisedb.p.rapidapi.com/bodyPart/${bodyPart}`, exerciseOptions);
+        }
+
+        setExercises(exercisesData);
+      }
+
+      {fetchExercisesData};
+    },[bodyPart])
+
   return (
     <Box id='exercises'
     sx={{ mt: { lg:'110px'}}}
