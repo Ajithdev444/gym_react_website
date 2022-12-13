c2cb424926d0b241d9aa437ab9509ebf831acff5
ajithdevarc <120442360+ajithdevarc@users.noreply.github.com>
2022-12-13 16:11:18 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
       <img src={Logo}  alt='logo' width='200px' height='40px'/>
       <Typography variant='h5' pb='40px' mt='20px'>
-        Made by Ajithdev 
+        Made by Ajithdev C
       </Typography>
     </Stack>
    </Box>
